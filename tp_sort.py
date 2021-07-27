a159f2de1e0f71c6a0efc59e7cca88a89f14bcb9
Thanh Binh (shanky) <72479657+bshankya93@users.noreply.github.com>
2021-07-27 18:26:30 +0700

    Upload xa phuong, quan huyen

---
+++ tp_sort.py
@@ -0,0 +1,14 @@
+import json
+from os import listdir
+from os.path import isfile, join
+f = open('tinh_tp.json')
+data = json.load(f)
+a = [{'code':  0, 'name': 'Vui lòng chọn một thành phố'}]
+for i in range(1,96):
+    for j in data:
+        if(int(data[j]['code']) == i):
+            a.append(data[j])
+
+oj = open('tp_sorted.json', 'w')
+oj.write(json.dumps(a))
+oj.close()
