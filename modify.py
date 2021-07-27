05699ba6cda8dde88b78db14bc206ec7ea2c9f9f
Thanh Binh (shanky) <72479657+bshankya93@users.noreply.github.com>
2021-07-27 19:11:41 +0700

    Upload xa phuong, quan huyen

--- modify.py
+++ modify.py
@@ -1,18 +1,19 @@
 import json
 from os import listdir
 from os.path import isfile, join
-mypath = 'quan-huyen'
-outpath = 'nk-qh'
+mypath = 'xa-phuong'
+outpath = 'nk-xp'
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-for file in onlyfiles:
-    fj = open(mypath+'/'+file)
+print('001.json' in onlyfiles)
+# for file in onlyfiles:
+#     fj = open(mypath+'/'+file)
 
-    print(file)
-    data = json.load(fj)
-    # a = [{'code': 0, 'name': 'Chọn QH'}]
-    a = []
-    for i in data:
-        a.append(data[i])
-    oj = open(outpath + '/' + file, 'w')
-    oj.write(json.dumps(a))
-    oj.close()
+#     print(file)
+#     data = json.load(fj)
+#     # a = [{'code': 0, 'name': 'Chọn QH'}]
+#     a = []
+#     for i in data:
+#         a.append(data[i])
+#     oj = open(outpath + '/' + file, 'w')
+#     oj.write(json.dumps(a))
+#     oj.close()
